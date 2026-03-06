dce52e7cd2ac837e9e030866606cf289a2aea5fe
null <60427892+null8626@users.noreply.github.com>
2026-03-07 00:47:51 +0700

    feat: remove User class which is only used in webhooks

--- DiscordBotsList.Api/Internal/User.cs
+++ DiscordBotsList.Api/Internal/User.cs
@@ -7,38 +7,6 @@ using DiscordBotsList.Api.Serialization;
 namespace DiscordBotsList.Api.Internal
 {
     /// <summary>
-    ///     A Top.gg user.
-    /// </summary>
-    public class User
-    {
-        /// <summary>
-        ///     The user's ID.
-        /// </summary>
-        [JsonPropertyName("id")]
-        [JsonConverter(typeof(ULongToStringConverter))]
-        public ulong Id { get; internal init; }
-
-        /// <summary>
-        ///     The user's name.
-        /// </summary>
-        [JsonPropertyName("name")]
-        public string Name { get; internal init; }
-
-        /// <summary>
-        ///     The user's avatar URL.
-        /// </summary>
-        [JsonPropertyName("avatar_url")]
-        public string Avatar { get; internal init; }
-
-        /// <summary>
-        ///     The user's platform ID.
-        /// </summary>
-        [JsonPropertyName("platform_id")]
-        [JsonConverter(typeof(ULongToStringConverter))]
-        public ulong PlatformId { get; internal init; }
-    }
-
-    /// <summary>
     ///     A user account from an external platform that is linked to a Top.gg user account.
     /// </summary>
     public enum UserSource
