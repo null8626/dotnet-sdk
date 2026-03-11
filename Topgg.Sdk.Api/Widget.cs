8dfc2f7db0ec0fb387ab93c65813a0fe3144bb09
null <60427892+null8626@users.noreply.github.com>
2026-03-11 10:19:11 +0700

    doc: remove redundant scoped namespace in Widget

--- Topgg.Sdk.Api/Widget.cs
+++ Topgg.Sdk.Api/Widget.cs
@@ -1,35 +1,34 @@
 using System.Text.RegularExpressions;
 using Topgg.Sdk.Api.Data;
 
-namespace Topgg.Sdk.Api
+namespace Topgg.Sdk.Api;
+
+public static partial class Widget
 {
-    public static partial class Widget
-    {
-        [GeneratedRegex("(?<!^)([A-Z])", RegexOptions.Compiled)]
-        private static partial Regex TypeConversionRegex();
+    [GeneratedRegex("(?<!^)([A-Z])", RegexOptions.Compiled)]
+    private static partial Regex TypeConversionRegex();
 
-        /// <summary>Generates a large widget URL.</summary>
-        /// <param name="type">The project's type.</param>
-        /// <param name="id">The project ID.</param>
-        /// <returns>The widget URL.</returns>
-        public static string Large(ProjectType type, ulong id) => $"{TopggApi.BaseURL}/widgets/large/{TypeConversionRegex().Replace(type.ToString(), "/$1").ToLower()}/{id}";
+    /// <summary>Generates a large widget URL.</summary>
+    /// <param name="type">The project's type.</param>
+    /// <param name="id">The project ID.</param>
+    /// <returns>The widget URL.</returns>
+    public static string Large(ProjectType type, ulong id) => $"{TopggApi.BaseURL}/widgets/large/{TypeConversionRegex().Replace(type.ToString(), "/$1").ToLower()}/{id}";
 
-        /// <summary>Generates a small widget URL for displaying votes.</summary>
-        /// <param name="type">The project's type.</param>
-        /// <param name="id">The project ID.</param>
-        /// <returns>The widget URL.</returns>
-        public static string Votes(ProjectType type, ulong id) => $"{TopggApi.BaseURL}/widgets/small/votes/{TypeConversionRegex().Replace(type.ToString(), "/$1").ToLower()}/{id}";
+    /// <summary>Generates a small widget URL for displaying votes.</summary>
+    /// <param name="type">The project's type.</param>
+    /// <param name="id">The project ID.</param>
+    /// <returns>The widget URL.</returns>
+    public static string Votes(ProjectType type, ulong id) => $"{TopggApi.BaseURL}/widgets/small/votes/{TypeConversionRegex().Replace(type.ToString(), "/$1").ToLower()}/{id}";
 
-        /// <summary>Generates a small widget URL for displaying a project's owner.</summary>
-        /// <param name="type">The project's type.</param>
-        /// <param name="id">The project ID.</param>
-        /// <returns>The widget URL.</returns>
-        public static string Owner(ProjectType type, ulong id) => $"{TopggApi.BaseURL}/widgets/small/owner/{TypeConversionRegex().Replace(type.ToString(), "/$1").ToLower()}/{id}";
+    /// <summary>Generates a small widget URL for displaying a project's owner.</summary>
+    /// <param name="type">The project's type.</param>
+    /// <param name="id">The project ID.</param>
+    /// <returns>The widget URL.</returns>
+    public static string Owner(ProjectType type, ulong id) => $"{TopggApi.BaseURL}/widgets/small/owner/{TypeConversionRegex().Replace(type.ToString(), "/$1").ToLower()}/{id}";
 
-        /// <summary>Generates a small widget URL for displaying social stats.</summary>
-        /// <param name="type">The project's type.</param>
-        /// <param name="id">The project ID.</param>
-        /// <returns>The widget URL.</returns>
-        public static string Social(ProjectType type, ulong id) => $"{TopggApi.BaseURL}/widgets/small/social/{TypeConversionRegex().Replace(type.ToString(), "/$1").ToLower()}/{id}";
-    }
+    /// <summary>Generates a small widget URL for displaying social stats.</summary>
+    /// <param name="type">The project's type.</param>
+    /// <param name="id">The project ID.</param>
+    /// <returns>The widget URL.</returns>
+    public static string Social(ProjectType type, ulong id) => $"{TopggApi.BaseURL}/widgets/small/social/{TypeConversionRegex().Replace(type.ToString(), "/$1").ToLower()}/{id}";
 }
